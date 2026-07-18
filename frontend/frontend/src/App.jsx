9e74b52505b1aa4ad72b59d0b8690517e53c8659
sumi <sumichasa72@gmail.com>
2026-07-18 20:26:53 +0530

    Fix import names for Vercel

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,24 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Profile from "./pages/Profile";
-import Home from "./pages/Home";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
+import Home from "./pages/home";
+import Register from "./pages/register";
+import Login from "./pages/login";
 
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-
 function App() {
-
   return (
-
     <BrowserRouter>
-
       <Navbar />
 
       <Routes>
-
         <Route
           path="/"
           element={
@@ -28,9 +23,8 @@ function App() {
           }
         />
 
-
         <Route
-          path="/profile"
+          path="/Profile"
           element={
             <ProtectedRoute>
               <Profile />
@@ -38,26 +32,12 @@ function App() {
           }
         />
 
+        <Route path="/register" element={<Register />} />
 
-        <Route 
-          path="/register" 
-          element={<Register />} 
-        />
-
-
-        <Route 
-          path="/login" 
-          element={<Login />} 
-        />
-
-
+        <Route path="/login" element={<Login />} />
       </Routes>
-
     </BrowserRouter>
-
   );
-
 }
-
 
 export default App;
